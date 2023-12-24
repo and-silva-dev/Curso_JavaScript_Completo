130313c6f0868e2490a2c267df5e43b26b0a7289
Anderson Silva <andersonsoufeliz27@gmail.com>
2023-12-24 16:49:22 -0300

    variáveis e tipos de dados

---
+++ tipos-de-dados/js.js
@@ -0,0 +1,25 @@
+/**
+ Variáveis
+var nome = 'Anderson';
+var totalComprado = 5;
+var preco = 20;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+* 
+ */
+//Tipos de dados
+
+var nome = "anderson";
+var idade = "50";
+var time = null;
+console.log(typeof time);
+console.log(typeof nome);
+
+//template String
+
+var gols = 1000;
+var frase1 = 'O jogador fez ' + gols + ' gols';
+var frase2 = `O jogador fez ${gols} gols`;
+
+console.log(frase1);
+console.log(frase2);
---
+++ variaveis/js.js
@@ -0,0 +1,13 @@
+/**
+ Variáveis
+var nome = 'Anderson';
+var totalComprado = 5;
+var preco = 20;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+* 
+ */
+//Tipos de dados
+
+var nome = 'anderson';
+console.log(typeof nome );
