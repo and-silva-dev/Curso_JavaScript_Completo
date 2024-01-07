60bf9753b8e775d8049b5cb6c6b8873493f11ca5
Anderson Silva <andersonsoufeliz27@gmail.com>
2024-01-07 19:53:18 -0300

    objeto parte 1

---
+++ objetos/js.js
@@ -0,0 +1,24 @@
+console.log("testin");
+var pessoa = {
+nome: "Anderson",
+idade: 36,
+}
+console.log(pessoa.idade);
+
+var quadrado = {
+  lados:4,
+  area: function(lado){
+    return lado*lado;
+  }, perimetro: function(lado){
+    return lado * this.lados;
+}
+}
+
+ 
+console.log(quadrado.area(8));
+console.log(quadrado.perimetro(5));
+
+console.log(Math.random());
+
+console.table(pessoa);
+console.table(quadrado);
